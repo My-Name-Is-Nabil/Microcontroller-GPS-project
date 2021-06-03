b666fcb4208c81edc242208ce052dbf8df0c6f91
ayatamer31 <54840865+ayatamer31@users.noreply.github.com>
2021-06-03 03:57:18 +0200

    added uart2 functions

--- UART.c
+++ UART.c
@@ -5,13 +5,15 @@ void UART0_Init(void)
 {
 	SYSCTL_RCGCUART_R |= 0x01; //activate UART0
 	SYSCTL_RCGCGPIO_R |= 0x01; //activate PORTA
+	while ((SYSCTL_PRGPIO_R & 0x01) == 0);
 	UART0_CTL_R &= ~0x01; //disable the UART
-	//BaudRate = 9600 --> 80MHZ /(16*9600) = 520.8333
-	UART0_IBRD_R = 520; //integer part = 520
-	UART0_FBRD_R = 53; //fractional part = 0.8333*64 = 53
+	//PC BaudRate = 115200 --> 80MHZ /(16*115200) = 43.402
+	UART0_IBRD_R = 43; //integer part = 43
+	UART0_FBRD_R = 26; //fractional part = 0.402*64 = 26
 	UART0_LCRH_R = 0x70; //8-bit data, no parity, one stop, FIFo.s enabled
 	UART0_CTL_R |= 0x01; //enable the UART
-	GPIO_PORTA_LOCK_R = 0x4C4F434B; //unlocking PORTA
+	//unlocking PORTA
+	GPIO_PORTA_LOCK_R = 0x4C4F434B;
 	GPIO_PORTA_CR_R |= 0xFF;
 	GPIO_PORTA_AFSEL_R |= 0x03; //enable alternate function for PA0 & PA1
 	GPIO_PORTA_DEN_R |= 0x03; //configure PA0 & PA1 as digital I/O
@@ -22,11 +24,57 @@ void UART0_Init(void)
 uint8_t UART0_Receive(void)
 {
 	while ((UART0_FR_R & 0x10) != 0); //waiting for new input as long as FIFO is empty
-	return UART0_DR_R; //read the received data
+	uint8_t data = UART0_DR_R; //read the received data
+	return data; //return the received data
 }
 
 void UART0_Send(uint8_t data)
 {
 	while ((UART0_FR_R & 0x20) != 0); //waiting for FIFO to NOT be full
 	UART0_DR_R = data;
+}
+
+void UART0_DeInit(void)
+{
+	UART0_CTL_R &= ~0x01; //disable the UART
+}
+
+
+
+void UART2_Init(void)
+{
+	SYSCTL_RCGCUART_R |= 0x04; //activate UART2
+	SYSCTL_RCGCGPIO_R |= 0x08; //activate PORTD
+	while ((SYSCTL_PRGPIO_R & 0x08) == 0);
+	UART2_CTL_R &= ~0x01; //disable the UART
+	//GPS BaudRate = 9600 --> 80MHZ /(16*9600) = 520.8333
+	UART2_IBRD_R = 520; //integer part = 520
+	UART2_FBRD_R = 53; //fractional part = 0.8333*64 = 53
+	UART2_LCRH_R = 0x70; //8-bit data, no parity, one stop, FIFo.s enabled
+	UART2_CTL_R |= 0x01; //enable the UART
+	//unlocking PORTD
+	GPIO_PORTD_LOCK_R = 0x4C4F434B;
+	GPIO_PORTD_CR_R |= 0xFF;
+	GPIO_PORTD_AFSEL_R |= 0xC0; //enable alternate function for PD6 & PD7
+	GPIO_PORTD_DEN_R |= 0xC0; //configure PD6 & PD7 as digital I/O
+	GPIO_PORTD_PCTL_R |= 0x22000000; //configure PD6 & PD7 as Rx & Rx of UART2
+	GPIO_PORTD_AMSEL_R &= ~0xC0; //disable analog on PD6 & PD7
+}
+
+uint8_t UART2_Receive(void)
+{
+	while ((UART2_FR_R & 0x10) != 0); //waiting for new input as long as FIFO is empty
+	uint8_t data = UART2_DR_R; //read the received data
+	return data; //return the received data
+}
+
+void UART2_Send(uint8_t data)
+{
+	while ((UART2_FR_R & 0x20) != 0); //waiting for FIFO to NOT be full
+	UART2_DR_R = data;
+}
+
+void UART2_DeInit(void)
+{
+	UART2_CTL_R &= ~0x01; //disable the UART
 }
--- UART.h
+++ UART.h
@@ -6,5 +6,11 @@
 void UART0_Init(void);
 uint8_t UART0_Receive(void);
 void UART0_Send(uint8_t);
+void UART0_DeInit(void);
+
+void UART2_Init(void);
+uint8_t UART2_Receive(void);
+void UART2_Send(uint8_t);
+void UART2_DeInit(void);
 
 #endif // __UART_H__
