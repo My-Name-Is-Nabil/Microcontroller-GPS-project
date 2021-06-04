3fb08b6c41b16c3fd67b71de40db4fc34bbda15e
Mohamedmoussa19999 <1701297@eng.asu.edu.eg>
2021-06-04 20:11:26 +0200

    7 segment

--- displacement.c
+++ displacement.c
@@ -1,15 +1,14 @@
 #include "stdint.h"
 #include "tm4c123gh6pm.h"
 #include "7-Segment-LED.h"
-void SystemInit();
 void sevensegment(int distance)
 {
-	int x,y,z,y2;
-	char x1,y1,z1;
-	x=distance%10;
-	y=((distance%100)-x)/10;
-	y2=distance%100;
-	z=(distance-y2)/100;
+	int x,y,z;
+	x = distance%10;
+	distance /= 10;
+	y = distance%10;
+	distance /= 10;
+	z = distance%10;
 
 	SEVENSEGMENT_Write_Units(x);
 	SEVENSEGMENT_Write_Tens(y);
